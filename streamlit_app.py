809c376308cb52127622024e30b2e282de54964f
Calvin Skalla <55516848+cjskalla@users.noreply.github.com>
2023-11-21 07:12:53 -0600

    added try function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,17 +26,20 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header('Fruity Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would like information about?')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        #take the json version of the response and normalize it
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        #output it on the screen as a table
+        streamlit.dataframe(fruityvice_normalized)
+        
+except URLError as e:
+    streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-#take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it on the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 #troubleshooting backstop
 streamlit.stop()
